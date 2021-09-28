c93dc1c38eef9891203af6e551c2c3367f89d3a8
HP-HP <HP-HP@HP>
2021-09-28 17:19:46 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int x, m=0, h=0, a;
+    cin >> x;
+    a = x * 2;
+    h = a / 60;
+    m = a % 60;
+    cout << h << " " << m << endl;
+}
+
---
+++ Lab1Python/Lab1Python.py
@@ -0,0 +1,6 @@
+x=int(input())
+x=x*2
+h=x/60
+m=x%60
+print(h)
+print(m)
