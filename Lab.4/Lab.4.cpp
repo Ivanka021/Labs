d93aeadb20b69be4cf051eb946318396efb426c1
Іванна Коновалюк <ivanka.konovalyuk.04@gmail.com>
2021-11-22 14:35:16 +0200

    Добавила Lab

---
+++ Lab.4/Lab.4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    double x = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        x = x + (1. / (i * (i + 2)));
+    }
+    cout << x;
+}
